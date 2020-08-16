09cc8510e52f66fe08aee15d43e4fdae430c9398
Neo Chan <netkiller@msn.com>
2020-08-16 18:43:09 +0800

    更新

--- mac.sh
+++ mac.sh
@@ -2,4 +2,5 @@ git submodule update
 #rm -rf ~/tmp/linux/
 mkdir -p ~/tmp/linux/
 cp -r images ~/tmp/linux/
+cp common/docbook.css ~/tmp/linux/
 xsltproc -o ~/tmp/linux/ docbook-xsl/docbook.mac.xsl book.xml
