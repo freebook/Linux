f762b46bbfbe5ecdad46b9cd57b57de3b750d765
Neo Chen <netkiller@msn.com>
2020-10-25 09:58:30 +0800

    调整章节

---
+++ Monitoring/mac.sh
@@ -0,0 +1,6 @@
+BASEDIR=~/tmp/monitor/
+#rm -rf ${BASEDIR}
+mkdir -p ${BASEDIR}
+cp -r images ${BASEDIR}
+cp ../common/docbook.css ${BASEDIR}
+xsltproc -o ${BASEDIR} ../docbook-xsl/docbook.mac.xsl book.xml
